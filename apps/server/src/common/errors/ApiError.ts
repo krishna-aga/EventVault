ee1335ce87949a2736d6d58ac1f448b3a03d5b19
krishna-aga <krishnaskb600@gmail.com>
2026-06-10 00:00:42 +0530

    issue 6

--- apps/server/src/common/errors/ApiError.ts
+++
@@ -1,9 +0,0 @@
-export class ApiError extends Error {
-  public readonly statusCode: number;
-
-  constructor(statusCode: number, message: string) {
-    super(message);
-    this.name = "ApiError";
-    this.statusCode = statusCode;
-  }
-}
